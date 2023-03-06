348b6d8090cea02e431361348719f4f156c0dc0a
Amdrii <andreyk557@gmail.com>
2023-03-06 17:15:58 +0200

    feat[new]

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
 
 
 const defaultOptions = {
-    linkClass: '.cardLink',
-    linkImageClass: '.cardImage',
+    linkClass: '.card',
 };
 
 const explosionClassName = 'explosion';
@@ -34,7 +33,56 @@ const explosionActiveImageClassName = 'explosionImage_Active';
 const explosionNextShowingImageClassName = 'explosionImage_NextShowing';
 const explosionNextHiddenImageClassName = 'explosionImage_NextHidden';
 
-class ExplositionGallery {}
+class ExplositionGallery {
+    constructor(elementNode, options){
+        this.options = {
+            ...defaultOptions,
+            ...options
+        };
+        this.containerNode = elementNode;
+        this.linkNodes = elementNode.querySelectorAll(this.linkClass)
+        
+        this.minWidth = 1023;
+        this.minHeigth = 600;
+        this.padding = 2 * 16;
+        this.showingCount = 4;
+        this.currentIndex = 0;
+
+        this.size = this.linkNodes.length;
+
+        this.initModal()
+    }
+    initModal() {
+        this.modalContainerNode = document.createElement("div");
+        this.modalContainerNode.className = explosionClassName;
+
+        this.modalContainerNode.innerHTML = `
+            <div class="${explosionSummaryClassName}">
+                <div  class="${explosionSummaryContentClassName}">
+                    <h2  class="${explosionTitleClassName}"></h2>
+                    <p  class="${explosionDescriptionClassName}"></p>
+                </div>
+            </div>
+            <div  class="${explosionControlsClassName}">
+                <button  class="${explosionCloseClassName}"></button>
+                <div  class="${explosionNavsClassName}">
+                    <button  class="${explosionNavClassName} ${explosionNavPrevClassName}"></button>
+                    <div  class="${explosionClassName}">
+                        1/${this.size}
+                    </div>
+                    <button  class="${explosionNavClassName} ${explosionNavNextClassName}"></button>
+                </div>
+            </div>
+            <div class="${explosionImagesClassName}">
+                ${Array.from(this.linkNodes).map((linkNode) => `
+                    <img src="${linkNode.getAttribute('href')}" alt="${linkNode.dataset.title}" class="${explosionImageClassName}" data-title="${linkNode.dataset.title}" data-description="${linkNode.dataset.description}" />
+                `).join('') }
+            </div>
+        `;
+
+        document.body.appendChild(this.modalContainerNode)
+    }
+}
 
 /**
  * Helpers
